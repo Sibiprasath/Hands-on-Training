be67efad1b1f95ecd349a781c54657bf77078eb5
Sibiprasath S <sibi45prasath@gmail.com>
2025-08-06 19:31:08 +0530

    push

--- righttriangle.java
+++ righttriangle.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class righttriangle {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        
-        for(int i = 1; i <= 4; i++) {
+        int n=sc.nextInt();
+        for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
